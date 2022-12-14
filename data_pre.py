5aed98e1097b6741b56ebd5c1f3e5b5a2e11acf3
nahyeon00 <nastela0326@gmail.com>
2022-12-14 12:35:47 +0900

    data_pre

---
+++ data_pre.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+filepath = "all_data.csv"
+
+df_data = pd.read_csv(filepath)
+
+df_file1 = df_data.sample(frac=0.8, random_state=2022).sort_index()
+df_file2 = df_data.drop(df_file1.index)
+
+df_file1.to_csv('/content/drive/MyDrive/NN/train1.csv', sep=',', na_rep='NaN', index=False)  # 혹시 몰라서 파일 이름을 train1로
+df_file2.to_csv('/content/drive/MyDrive/NN/test.csv', sep=',',na_rep='NaN', index = False)
